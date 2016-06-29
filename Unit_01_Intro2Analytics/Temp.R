8afbff847e059815754dc874f810b6d43440c12a
Miraj Vashi <miraj.vashi@gmail.com>
2016-06-29 22:22:51 +0530

    COMMIT1_06292016

---
+++ Unit_01_Intro2Analytics/Temp.R
@@ -0,0 +1,10 @@
+dfGrade1 <- read.csv("../Data/Grade1.csv", header = TRUE)
+dfGrade2 <- read.csv("../Data/Grade2.csv", header = TRUE)
+
+dim(dfGrade1)
+dim(dfGrade2)
+
+head(dfGrade1)
+train(dfGrade2)
+
+
